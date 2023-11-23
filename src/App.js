ece27c2246fc1ae583b3ba3177116c41679e0633
Oleksandr Kokhno <kohnoan@gmail.com>
2023-11-23 22:45:33 +0200

    add Promo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Promo from './components/promo/Promo';
 import Header from './components/header/Header';
 
 export const App = () => {
   return (
     <div>
       <Header />
+      <Promo />
     </div>
   );
 };
